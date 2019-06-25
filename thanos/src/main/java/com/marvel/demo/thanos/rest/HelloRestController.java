4941aae912298e624cf12d0f3d47f5e3eceaa6f2
fuyongde <fuyongde@zhuifintech.com>
2019-06-25 19:03:13 +0800

    feat: 新增docker 打包 Spring Boot Application 的demo

---
+++ thanos/src/main/java/com/marvel/demo/thanos/rest/HelloRestController.java
@@ -0,0 +1,18 @@
+package com.marvel.demo.thanos.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/hello")
+public class HelloRestController {
+
+    @GetMapping
+    public String hello(
+        @RequestParam(name = "name", required = false) String name
+    ) {
+        return "Hello " + name;
+    }
+}
